6622b6bae46d8328f97b5dc80eb5ab11b074caf1
Leon Vintzeanu <leonvintzeanu@protonmail.com>
2026-08-05 22:22:31 +0300

    remove vertical padding from navbar items

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -16,10 +16,10 @@
 <div class="min-h-screen bg-surface-50-950 text-primary-800-200 transition-colors">
   <AppBar class="shadow-text-700-300 sticky top-0 z-40 bg-surface-100-900 shadow-md backdrop-blur">
     <AppBar.Toolbar class="flex justify-between">
-      <AppBar.Lead class="p-4">
+      <AppBar.Lead class="px-4">
         <a href={resolve('/')} class=" text-xl font-bold text-primary-500">Pokemon Finder</a>
       </AppBar.Lead>
-      <AppBar.Trail class="p-4">
+      <AppBar.Trail class="px-4">
         <Lightswitch />
       </AppBar.Trail>
     </AppBar.Toolbar>
