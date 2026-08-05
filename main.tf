4aeea3e1a71e2a6f29bb2382f260c25e3bbafef8
Leon Vintzeanu <leonvintzeanu@protonmail.com>
2026-08-05 23:25:13 +0300

    fix dockerfile, update terraform config with new ui container

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ locals {
   nginx_config            = file("${path.module}/nginx/nginx.conf")
   nginx_config_mount_path = "/etc/nginx/conf.d/default.conf"
   nginx_image_tag         = "nginx:1.31.3"
+  hh_ui_build_context     = "${path.module}/ui"
   hh_api_build_context    = "${path.module}/api"
 }
 
@@ -23,15 +24,46 @@ resource "docker_network" "api_net" {
   driver = "bridge"
 }
 
-# nginx container
-resource "docker_image" "nginx" {
-  name         = local.nginx_image_tag
+# # nginx container
+# resource "docker_image" "nginx" {
+#   name         = local.nginx_image_tag
+#   keep_locally = false
+# }
+
+# resource "docker_container" "nginx" {
+#   image = docker_image.nginx.image_id
+#   name  = "nginx"
+
+#   ports {
+#     internal = 80
+#     external = 8000
+#   }
+
+#   upload {
+#     content = local.nginx_config
+#     file    = local.nginx_config_mount_path
+#   }
+
+#   networks_advanced {
+#     name = docker_network.api_net.name
+#   }
+
+#   depends_on = [docker_container.hh_api]
+# }
+
+# ui container
+resource "docker_image" "hh_ui" {
+  name         = "hh-ui"
   keep_locally = false
+
+  build {
+    context = local.hh_ui_build_context
+  }
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "nginx"
+resource "docker_container" "hh_ui" {
+  image = docker_image.hh_ui.image_id
+  name  = "hh_ui"
 
   ports {
     internal = 80
