64c4186bf357a90137be160ab1a507d91af8da5b
Leon Vintzeanu <leonvintzeanu@protonmail.com>
2026-08-06 18:09:00 +0300

    create ingress variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,3 +6,21 @@ locals {
   ingress_cert_validtity_hours = 2160 # valid for 90 days
   ingress_early_renewal_hours  = 720  # window for renewal
 }
+
+variable "ingress_hostname" {
+  type        = string
+  description = "Public facing hostname or IP for the Nginx ingress and self-signed TLS certificate"
+  default     = "localhost"
+}
+
+variable "ingress_http_port" {
+  type        = string
+  description = "HTTP listen port for the Nginx ingress"
+  default     = 8080
+}
+
+variable "ingress_https_port" {
+  type        = string
+  description = "HTTPS listen port for the Nginx ingress"
+  default     = 8443
+}
